d65ae266bbffef3b60c732015d3e7f377c98d907
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2021-02-24 18:58:41 +0200

    viime tunti

---
+++ Assets/Omat_jutut/Scripts/playeroma.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playeroma : MonoBehaviour
+{
+    public Animator anim;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame //No its not.
+    void Update()
+    {
+        CharacterController hahmokontrolleri = GetComponent<CharacterController>();
+
+        float horizontal = Input.GetAxis("Horizontal") * 5;
+        float vertical = Input.GetAxis("Vertical") * 5;
+
+        Vector3 nopeus = new Vector3(horizontal, 0, vertical);
+        //SimpleMove(nopeus);
+
+        hahmokontrolleri.Move(nopeus * Time.deltaTime);
+
+           if (Input.GetAxis("Vertical") != 0)
+		   { 
+		       anim.SetBool("walk", false); 
+		   } 
+		   else 
+		   { 
+		      anim.SetBool("walk", false);
+	}          }
+}          
+        
