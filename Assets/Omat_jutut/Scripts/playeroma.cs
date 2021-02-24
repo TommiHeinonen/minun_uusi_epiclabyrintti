d2442d77d9c3b3be411997d0d737b821d21fa144
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2021-02-24 19:55:00 +0200

    liikkuminen ja hiirellä kääntö koodattu

--- Assets/Omat_jutut/Scripts/playeroma.cs
+++ Assets/Omat_jutut/Scripts/playeroma.cs
@@ -5,33 +5,39 @@ using UnityEngine;
 public class playeroma : MonoBehaviour
 {
     public Animator anim;
+	private float horisontaalinenPyorinta = 0;
+    // Start is called before the first frame update // tämä oli järkevä elämän tarkoitus. // Button is a button which can be color. U can press it.
 	
-    // Start is called before the first frame update
+	//sä olet vakooja jos olet täällä
+	
     void Start()
     {
-        
-    }
+       anim = GetComponentInChildren<Animator>();
+	   }
 
     // Update is called once per frame //No its not.
     void Update()
     {
         CharacterController hahmokontrolleri = GetComponent<CharacterController>();
-
+        //Eteen ja sivulle liikkuminen
         float horizontal = Input.GetAxis("Horizontal") * 5;
         float vertical = Input.GetAxis("Vertical") * 5;
-
         Vector3 nopeus = new Vector3(horizontal, 0, vertical);
-        //SimpleMove(nopeus);
-
         hahmokontrolleri.Move(nopeus * Time.deltaTime);
-
+		
+		
+		horisontaalinenPyorinta += Input.GetAxis("Mouse X");
+		
+        transform.localRotation = Quaternion.Euler(0, horisontaalinenPyorinta, 0);
            if (Input.GetAxis("Vertical") != 0)
 		   { 
-		       anim.SetBool("walk", false); 
+		       anim.SetBool("walk1", true); 
 		   } 
 		   else 
 		   { 
-		      anim.SetBool("walk", false);
-	}          }
+		      anim.SetBool("walk1", false);
+			  
+	       }
+	}
 }          
-        
+        //sä olet vakooja jos olet täällä//sä olet vakooja jos olet täällä//sä olet vakooja jos olet täällä//sä olet vakooja jos olet täällä//sä olet vakooja jos olet täällä
